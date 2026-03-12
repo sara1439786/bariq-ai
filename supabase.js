4973662121da8d978d0cbed7602c1cbfee3b0c4f
sara1439786 <sara1439@gmail.com>
2026-03-12 06:18:08 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = "https://ennjdytlptaywokkzvnm.supabase.co"
-const supabaseKey = sb_publishable_npll8QpLghinRC6gMO4_wA_ZgM-ogRd 
+const supabaseUrl = 'https://ennjdytlptaywokkzvnm.supabase.co'
+const supabaseKey = 'sb_publishable_npll8QpLghinRC6gMO4_wA_ZgM-ogRd'
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
